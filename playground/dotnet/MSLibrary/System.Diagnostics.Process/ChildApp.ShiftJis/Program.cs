74db3a61f176808549e30219bafd3f61de6fb2d7
轟 尚冴 <n4304@ndensan.co.jp>
2026-01-05 10:45:41 +0900

    Add ProcessEncoding.Tests
    親プロセスが文字コードを指定し、それ以外の文字コードを子プロセスが指定した場合文字化けする。

---
+++ playground/dotnet/MSLibrary/System.Diagnostics.Process/ChildApp.ShiftJis/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+namespace dojo.playground.dotnet.ChildApp.ShiftJis;
+class Program
+{
+    static void Main()
+    {
+        // 明示的に Shift-JIS（CP932）
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        Console.OutputEncoding = Encoding.GetEncoding("shift_jis");
+
+        Console.WriteLine("日本語テスト：子プロセスはShift-JIS");
+    }
+}
---
+++ playground/dotnet/MSLibrary/System.Diagnostics.Process/ProcessEncoding.Tests/ProcessShiftJisTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Xunit;
+
+public class ProcessShiftJisTests
+{
+    [Fact]
+    public void ChildProcess_ShiftJisOutput_IsDecodedCorrectly()
+    {
+        var txtWriter = new StreamWriter("output.txt");
+        Console.SetOut(txtWriter);
+        var utf8 = Encoding.UTF8;
+
+        var psi = new ProcessStartInfo
+        {
+            FileName = "dotnet",
+            Arguments = "./ChildApp.ShiftJis.dll",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+
+            StandardOutputEncoding = utf8,
+            StandardErrorEncoding = utf8
+        };
+
+        var proc = new Process
+        {
+            StartInfo = psi,
+        };
+
+        proc.OutputDataReceived += (_, e) => { if (e.Data != null) Console.Out.WriteLine(e.Data); };
+        proc.ErrorDataReceived += (_, e) => { if (e.Data != null) Console.Error.WriteLine(e.Data); };
+
+        // プロセス開始
+        proc.Start();
+
+        // 出力の非同期読み取り開始
+        proc.BeginOutputReadLine();
+        proc.BeginErrorReadLine();
+
+        // プロセス終了を待機
+        proc.WaitForExit();
+
+        txtWriter.Close();
+
+        var text = File.ReadAllText("output.txt");
+        Assert.DoesNotContain("日本語テスト", text);
+    }
+}
